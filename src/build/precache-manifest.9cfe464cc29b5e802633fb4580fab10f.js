6ff74495818fefb09fb1893239c89a4c0453890b
Matthew Irving <matt@develop-matt.com>
2020-09-16 19:11:40 -0400

    First commit

---
+++ src/build/precache-manifest.9cfe464cc29b5e802633fb4580fab10f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58f3917f23c2fb41362ec22c88fe882",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c1a5dd9966172b1194c",
+    "url": "/static/css/2.c11e63b2.chunk.css"
+  },
+  {
+    "revision": "a947784988feed8dd412",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8c1a5dd9966172b1194c",
+    "url": "/static/js/2.bf6012c4.chunk.js"
+  },
+  {
+    "revision": "293868adea80fba0bc2a854a45223b2f",
+    "url": "/static/js/2.bf6012c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a947784988feed8dd412",
+    "url": "/static/js/main.4898d654.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
